a464b1369d56cee2fabb4f8bca0ad30ffb957fb3
ali rostami <hidden105@gmail.com>
2017-12-19 20:03:19 +0330

    initial commit

---
+++ assets/js/functions.js
@@ -0,0 +1,152 @@
+//$(function(){});
+$(window).scroll(function(){
+    parallax();
+});
+
+
+function parallax() {
+    var wScroll = $(window).scrollTop();
+    $('.parallax-bg').css('background-position','center '+ (wScroll*0.55) +'px')
+}
+
+$(window).scroll(function () {
+   aparatVidScroll();
+});
+
+function aparatVidScroll() {
+    var wScroll = $(window).scrollTop();
+
+    $('.video-strip').css('background-position','center -'+  wScroll +'px');
+}
+
+
+$(document).ready(function(){
+    $('a[href^="#"]').on('click',function (e) {
+        e.preventDefault();
+
+        var target = this.hash;
+        $target = $(target);
+
+        $('html, body').stop().animate({
+            'scrollTop':  $target.offset().top
+        }, 900, 'swing', function () {
+            window.location.hash = target;
+        });
+    });
+});
+
+
+$(document).ready(function(){
+    // $fn.scrollSpeed(step, speed, easing);
+    jQuery.scrollSpeed(100, 800);
+});
+
+// Custom scrolling speed with jQuery
+// Source: github.com/ByNathan/jQuery.scrollSpeed
+// Version: 1.0.2
+
+(function($) {
+
+    jQuery.scrollSpeed = function(step, speed, easing) {
+
+        var $document = $(document),
+            $window = $(window),
+            $body = $('html, body'),
+            option = easing || 'default',
+            root = 0,
+            scroll = false,
+            scrollY,
+            scrollX,
+            view;
+
+        if (window.navigator.msPointerEnabled)
+
+            return false;
+
+        $window.on('mousewheel DOMMouseScroll', function(e) {
+
+            var deltaY = e.originalEvent.wheelDeltaY,
+                detail = e.originalEvent.detail;
+            scrollY = $document.height() > $window.height();
+            scrollX = $document.width() > $window.width();
+            scroll = true;
+
+            if (scrollY) {
+
+                view = $window.height();
+
+                if (deltaY < 0 || detail > 0)
+
+                    root = (root + view) >= $document.height() ? root : root += step;
+
+                if (deltaY > 0 || detail < 0)
+
+                    root = root <= 0 ? 0 : root -= step;
+
+                $body.stop().animate({
+
+                    scrollTop: root
+
+                }, speed, option, function() {
+
+                    scroll = false;
+
+                });
+            }
+
+            if (scrollX) {
+
+                view = $window.width();
+
+                if (deltaY < 0 || detail > 0)
+
+                    root = (root + view) >= $document.width() ? root : root += step;
+
+                if (deltaY > 0 || detail < 0)
+
+                    root = root <= 0 ? 0 : root -= step;
+
+                $body.stop().animate({
+
+                    scrollLeft: root
+
+                }, speed, option, function() {
+
+                    scroll = false;
+
+                });
+            }
+
+            return false;
+
+        }).on('scroll', function() {
+
+            if (scrollY && !scroll) root = $window.scrollTop();
+            if (scrollX && !scroll) root = $window.scrollLeft();
+
+        }).on('resize', function() {
+
+            if (scrollY && !scroll) view = $window.height();
+            if (scrollX && !scroll) view = $window.width();
+
+        });
+    };
+
+    jQuery.easing.default = function (x,t,b,c,d) {
+
+        return -c * ((t=t/d-1)*t*t*t - 1) + b;
+    };
+
+})(jQuery);
+
+//  create a menu
+$(function() {
+    $('#menu').trigger("open").mmenu({
+        // options
+
+    }, {
+        // configuration
+        menuNodetype: "nav"
+
+    });
+});
